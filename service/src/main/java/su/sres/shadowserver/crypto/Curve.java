d3d861b145fb3f931c9c7504c5c504bb6094b6de
Anton Alipov <alipov@ieee.org>
2022-08-15 19:04:15 +0300

    Update copyright statement

--- service/src/main/java/su/sres/shadowserver/crypto/Curve.java
+++ service/src/main/java/su/sres/shadowserver/crypto/Curve.java
@@ -1,3 +1,8 @@
+/*
+ * Original software: Copyright 2013-2020 Signal Messenger, LLC
+ * Modified software: Copyright 2019-2022 Anton Alipov, sole trader
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 package su.sres.shadowserver.crypto;
 
 import org.whispersystems.curve25519.Curve25519;
--- service/src/main/java/su/sres/shadowserver/metrics/TrafficSource.java
+++ service/src/main/java/su/sres/shadowserver/metrics/TrafficSource.java
@@ -1,3 +1,8 @@
+/*
+ * Original software: Copyright 2013-2020 Signal Messenger, LLC
+ * Modified software: Copyright 2019-2022 Anton Alipov, sole trader
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 package su.sres.shadowserver.metrics;
 
 public enum TrafficSource {
